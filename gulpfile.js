76e2aa9abaf5afc5d27165d4948bf03348b1e0ee
Dan Stineback <dstineback@gmail.com>
2016-11-04 10:12:46 -0700

    starting the form project. just feeling out the html at this point. Will need to modify over to angular to make more dynamic once the DB is figured out.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+
+const paths = {
+  js: __dirname + '/app/**/*.js',
+  html: __dirname + '/app/index.html',
+  css: __dirname + '/app/**/*.css'
+};
+
+gulp.task('clean', ()=>{
+  return gulp.src('./build/*', {read:false})
+    .pipe(clean());
+});
+
+gulp.task('copy-html', ['clean'], ()=>{
+  return gulp.src(paths.html)
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('copy-css', ['clean'], ()=>{
+  return gulp.src(paths.css)
+    .pipe(gulp.dest('./build'));
+});
+
+
+
+gulp.task('watch', ()=>{
+  gulp.watch('./app/*', ['build']);
+});
+
+gulp.task('build', ['clean', 'copy-css', 'copy-html']);
+
+gulp.task('default', ['build']);
