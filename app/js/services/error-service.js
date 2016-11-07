ca3e334053db8868355fc3b740aab59993f023dc
Dan Stineback <dstineback@gmail.com>
2016-11-07 08:18:01 -0800

    added a error handling service and added a link to directive index.js

---
+++ app/js/services/error-service.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(app) {
+  app.factory('ErrorService', function($location, $window){
+    const service = {};
+    const errors = [];
+
+    service.logError = function(message) {
+      return function(err){
+        errors.push(message);
+        console.log(err);
+        $location.url('./');
+        $window.alert('Please log in');
+      };
+    };
+
+    service.getErrors = function() {
+      return errors;
+    };
+
+    return service;
+
+  });
+};
